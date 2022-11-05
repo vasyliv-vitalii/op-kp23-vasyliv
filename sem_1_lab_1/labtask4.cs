a33c8a800a64c91bfdd0302102ba71057318524b
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 18:05:02 +0200

    Added test scenarios

--- sem_1_lab_1/labtask4.cs
+++ sem_1_lab_1/labtask4.cs
@@ -2,6 +2,19 @@ class Programm
 {
     static void Main(string[] args)
     {
+        /*
+        test scenarios
+        test 1
+        x = 1 , a = 7 : sin = 0,8414709848078937;
+        test 2
+        x = 4 , a = 7 : sin = -0,7568024953079282;
+        test 3
+        x = 141 , a = 7 : sin = 0,36317127391807535;
+        test 4
+        x = 6 , a = 7 : sin = -0,2794153423357333;
+        test 5
+        x = 10 , a = 7 : sin = -0,54402111088937;
+        */
         double x;
         double a;
         double sin = 0;
