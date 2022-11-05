f76ae3d55bd47c47621b321ff303f1bda7937ff0
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 17:49:54 +0200

    Added test scenarios

--- sem_1_lab_1/labtask3.cs
+++ sem_1_lab_1/labtask3.cs
@@ -2,6 +2,19 @@ class Program
 {
     public static void Main(string[] args)
     {
+        /*
+        test scenarios 
+        test 1 
+        n = 1 , x = 2 : result - x^n = 1 , fatorial = 1;
+        test 2 
+        n = 5 , x = 6 : result - x^n = 7776 , fatorial = 120;
+        test 3 
+        n = 10 , x = 4 : result - x^n = 1048576 , fatorial = 1;
+        test 4 
+        n = -5 , x = 4 : result - x^n = 0,0009765625 , fatorial = answer doesn't exist;
+        test 5 
+        n = 4 , x = -2 : result - x^n = 16 , fatorial = 24;
+        */
         int n = 1, x = 1;
         double factorial = 1;
         Console.WriteLine(x);
