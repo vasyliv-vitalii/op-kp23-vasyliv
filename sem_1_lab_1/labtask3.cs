0dad59acf3609f1a7b03c44199e7b514d876d8ae
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 17:40:32 +0200

    Programm on plugs level

---
+++ sem_1_lab_1/labtask3.cs
@@ -0,0 +1,10 @@
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = 1, x = 1;
+        double factorial = 1;
+        Console.WriteLine(x);
+        Console.WriteLine(factorial);
+    }
+}
