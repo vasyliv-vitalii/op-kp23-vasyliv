f09304ee6a0d75b1c669725f659e1817e7896274
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 17:57:52 +0200

    Programm on plugs level

---
+++ sem_1_lab_1/labtask4.cs
@@ -0,0 +1,24 @@
+class Programm
+{
+    static void Main(string[] args)
+    {
+        double x;
+        double a;
+        double sin = 0;
+        int sign =0;
+        Console.WriteLine(sin * sign);
+
+
+    }
+    static double Power(double numb, double n)
+    {
+        double res = 1;
+        return res;
+    }
+    static double Factorial(double numb)
+    {
+        double factorial = 1;
+        return factorial;
+    }
+
+}
