01997e915ced8582fb0bb63783e5524252b9074a
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 17:32:03 +0200

    Full implementation of code

--- sem_1_lab_1/labtask2.cs
+++ sem_1_lab_1/labtask2.cs
@@ -15,8 +15,27 @@ class Program
          test 4
          n = 1789 - the number is prime;
          */
-        double n = 0;
-        Console.WriteLine(n);
+        double n;
+        Console.WriteLine("¬ведiть число n");
+        n = Convert.ToDouble(Console.ReadLine());
+        if (n < 0)
+        {
+            Console.WriteLine("¬ведiть коректне число");
+        }
+        else
+        {
+            for (int i = 2; i < n; i++)
+            {
+                double res = n % i;
+
+                if (res == 0)
+                {
+                    Console.WriteLine("÷е число не Ї простим");
+                    return;
+                }
+            }
+            Console.WriteLine("„исло Ї простим");
+        }
 
     }
 }
