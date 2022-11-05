cc2bdb8f4d9b45d76639a9d74cdaef468f0b18fb
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 17:30:50 +0200

    Added test scenarios

--- sem_1_lab_1/labtask2.cs
+++ sem_1_lab_1/labtask2.cs
@@ -4,8 +4,19 @@ class Program
 {
     public static void Main(string[] args)
     {
+        /*
+         test scenarios
+         test 1 
+         n = 5 - the number is prime;
+         test 2
+         n = 40 - the number is not prime;
+         test 3
+         n = 123 - the number is not prime;
+         test 4
+         n = 1789 - the number is prime;
+         */
         double n = 0;
         Console.WriteLine(n);
-      
+
     }
 }
