efdb42cf584dcdc003351c637d36641172f1cca5
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 16:59:17 +0200

    Programm on plugs level

---
+++ sem_1_lab_1/labtask1.cs
@@ -0,0 +1,14 @@
+class Programm
+{
+    static void Main(string[] args)
+    {
+        double a = 1.35;
+        double b = -6.25;
+        double dx = 0.25;
+        double xk = 10.3;
+        double y = 0, x = 0;
+        Console.WriteLine("x =" + x + "   " + "y =" + y);
+    }
+
+
+}
