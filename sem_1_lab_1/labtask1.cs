f1d8501d0667fb7be9e48c41c44c64a4288d3ec0
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2022-11-05 17:06:59 +0200

    Added test scenarios

--- sem_1_lab_1/labtask1.cs
+++ sem_1_lab_1/labtask1.cs
@@ -2,6 +2,14 @@ class Programm
 {
     static void Main(string[] args)
     {
+        /*
+        test scenarios
+        test1 x = 5,3 , y = 201,12279953882054;
+        test2 x = 5,55 , y = 230,87287945419033;
+        test3 x = 5,8 , y = 230,87287945419033;
+        test4 x = 6,05 , y = 298,95585864547627;
+        test5 x = 6,3 , y = 337,6281553330734;
+         */
         double a = 1.35;
         double b = -6.25;
         double dx = 0.25;
