e4bbd48075d5ca6dc13a2c37f6416ae9bed3371e
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2023-02-21 20:00:12 +0200

    Full implementation of code

--- sem_2_lab_1/Task1.cs
+++ sem_2_lab_1/Task1.cs
@@ -2,9 +2,15 @@ class MyClass
 {
     static void Main(string[] args)
     {
-        string roadtofile = "";
+        string roadtofile = "D:\\123232323\\lab3task1\\test.txt";
         FileStream file = new FileStream(roadtofile, FileMode.Create);
+        file.Close();
         StreamWriter sw = new StreamWriter(roadtofile);
+        sw.WriteLine("Hello word");
+        sw.WriteLine("Hello Friends");
+        sw.Close();
         StreamReader sr = new StreamReader(roadtofile);
+        Console.WriteLine(sr.ReadToEnd());
+        sr.Close();
     }
 }
