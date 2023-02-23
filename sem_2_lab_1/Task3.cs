821f1101d1c8b37d3d0d24681769e9fe7eb52502
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2023-02-23 08:37:58 +0200

    Programm on plugs level

---
+++ sem_2_lab_1/Task3.cs
@@ -0,0 +1,13 @@
+class Class
+{
+    string path1 = "D:\\123232323\\Lab3Task3\\Lab3Task3\\TextFile1.txt";
+    string path2 = "D:\\123232323\\Lab3Task3\\Lab3Task3\\TextFile2.txt";
+    StreamReader sr = new StreamReader(path1);
+    int length = 0;
+    string[] arrayofwords = new string[length];
+    StreamReader sr1 = new StreamReader(path1)
+        ShellSort(arrayofwords);
+    StreamWriter sw1 = new StreamWriter(path2);
+    StreamReader sr2 = new StreamReader(path2);
+    Console.WriteLine(sr2.ReadToEnd());
+}
