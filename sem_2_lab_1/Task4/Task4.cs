47a54d004b096c1060195f2e124dfb190e4e74d9
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2023-02-24 09:42:33 +0200

    Added test scenarios

--- sem_2_lab_1/Task4/Task4.cs
+++ sem_2_lab_1/Task4/Task4.cs
@@ -1,5 +1,20 @@
-class MyClass
+﻿class MyClass
 {
+    /*
+     * Test scenarios:                                       Output:
+     *  Input: John,Rembo,20                                 John Rembo 20                   
+        Michel,Gordon,99                                     Toni Crack 30
+        Jame,Coock,100                                       Laura Brown 40   
+        Toni,Crack,30                                        Sara Black 59
+        Danil,Hunter,60                                      Caren Nakamura 52
+        Laura,Brown,40                                       Eva Dupont 59
+        Sara,Black,59                                        Olivia Gonzalez 44
+        Maria,Johnson,64
+        Caren,Nakamura,52
+        Eva,Dupont,59
+        Emma,Müller,70
+        Olivia,Gonzalez,44
+     */
     static void Main(string[] args)
     {
         string path1 = "D:\\kpi23\\op-kp23-vasyliv\\sem_2_lab_1\\Task4\\File1.csv";
