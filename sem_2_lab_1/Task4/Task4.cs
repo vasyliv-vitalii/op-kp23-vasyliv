ed62a18a9b10df47b8cbbd7aca7bc6f09d5dff67
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2023-02-24 09:35:30 +0200

    Programm on plugs level

---
+++ sem_2_lab_1/Task4/Task4.cs
@@ -0,0 +1,22 @@
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        string path1 = "D:\\kpi23\\op-kp23-vasyliv\\sem_2_lab_1\\Task4\\File1.csv";
+        StreamReader sr = new StreamReader(path1);
+        int length = 0;
+    }
+
+    static string[] Splits(string text, char symbol)
+    {
+        string[] splits = new string[3];
+        return splits;
+    }
+
+    static int IntParses(string numb)
+    {
+        int res = 0;
+        return res;
+    }
+
+}
