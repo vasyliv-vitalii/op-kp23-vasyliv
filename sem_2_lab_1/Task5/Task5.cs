a2caddb63ec8ffebf0efed063df7fc5566458573
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2023-02-26 10:39:58 +0200

    Programm on plugs level

---
+++ sem_2_lab_1/Task5/Task5.cs
@@ -0,0 +1,33 @@
+class MyClass
+{
+    static void Main(string[] args)
+    {
+        string path = @"D:\kpi23\op-kp23-vasyliv\sem_2_lab_1\Task5\File1.txt";
+        StreamReader sr = new StreamReader(path);
+    }
+
+    static (string, int) HowMuchTime(string word, string[] arr)
+    {
+        string name = "";
+        int result = 0;
+        return (name, result);
+    }
+    static int LengthOfArray(string text)
+    {
+        int length = 0;
+        string word = "";
+        return length;
+    }
+    static string ToLowers(string s)
+    {
+        string answer = "";
+        return answer;
+    }
+
+    static string[] Splits(string text, int length)
+    {
+        string[] answer = new string[length];
+        string word = "";
+        return answer;
+    }
+}
