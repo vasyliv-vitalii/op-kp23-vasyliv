1ace1050dbc8c545b9fff09055c4b1aeb67ebad9
vasyliv.vv123@gmail.com <vasyliv.vv123@gmail.com>
2023-04-15 23:53:44 +0300

    full implementation of code

--- sem_2_lab_2/task3/vector.cs
+++ sem_2_lab_2/task3/vector.cs
@@ -11,11 +11,26 @@ namespace lab2task3
         public int[] vector;
         public Vector(params int[] cordinates)
         {
+            vector = cordinates.ToArray();
         }
 
         public static int operator +(Vector vec1, Vector vec2)
         {
-            int sum = 0;  
+            int sum = 0;
+            foreach (var item in vec1.vector)
+            {
+                if (item < 0)
+                {
+                    sum += item;
+                }
+            }
+            foreach (var item in vec2.vector)
+            {
+                if (item < 0)
+                {
+                    sum += item;
+                }
+            }
             return sum;
         }
 
@@ -23,13 +38,41 @@ namespace lab2task3
         {
             int prod = 0;
             int index1 = 1;
-            int index2 = 1;          
+            int index2 = 1;
+            for (int i = 0, j = 0; i < vec1.vector.Length; i++)
+            {
+                if (j < vec2.vector.Length)
+                {
+                    if (index2 % 2 == 0 && index1 % 2 == 0)
+                    {
+                        prod += vec1.vector[i] * vec2.vector[j];
+                    }
+                    index2++;
+                    index1++;
+                    j++;
+                }
+
+            }
             return prod;
         }
 
         public static int operator -(Vector vec1, Vector vec2)
         {
-            int sum = 0;            
+            int sum = 0;
+            foreach (var item in vec1.vector)
+            {
+                if (item == 0)
+                {
+                    sum++;
+                }
+            }
+            foreach (var item in vec2.vector)
+            {
+                if (item == 0)
+                {
+                    sum++;
+                }
+            }
             return sum;
         }
     }
